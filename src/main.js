b17423774976f63771ceb4ecad45ff4a88e3e16c
JeanCarlos80s <jeancarlos80s@gmail.com>
2020-02-26 18:39:41 -0300

    Adicionando Vue Router

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import router from '@/router'
+
 import 'spectre.css/dist/spectre.min.css'
 import 'spectre.css/dist/spectre-icons.min.css'
 
@@ -8,4 +10,5 @@ Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  router,
 }).$mount('#app')
